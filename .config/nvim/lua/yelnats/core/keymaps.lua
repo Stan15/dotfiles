8f69550fcc971c747f72fb021145e6bf886ccb9f
Stanley Ihesiulo <stanihe1901@gmail.com>
2025-01-24 00:32:51 -0500

    fixed conflicting keymaps for coppying line references, general cleanup

--- .config/nvim/lua/yelnats/core/keymaps.lua
+++ .config/nvim/lua/yelnats/core/keymaps.lua
@@ -12,13 +12,6 @@ keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }
 keymap.set({ "n", "v" }, "<leader>y", '"+y', { noremap = true, desc = "Copy to system clipboard" })
 keymap.set({ "n", "v" }, "<leader>p", '"+p', { noremap = true, desc = "Paste from system clipboard" })
 
-function insertFullPath()
-	local filepath = vim.fn.expand("%")
-	vim.fn.setreg("+", filepath) -- write to clippoard
-end
-
-keymap.set("n", "<leader>pc", insertFullPath, { noremap = true, silent = true })
-
 -- keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
 -- keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
 -- keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
--- .config/nvim/lua/yelnats/plugins/future-use/float-edit.lua
+++
@@ -1,3 +0,0 @@
-return {
-	"metalelf0/nvim-floatedit",
-}
