756f4b1356c66066d2b5ef3c14dfbedb9937a32b
Stanley Ihesiulo <stanihe1901@gmail.com>
2025-07-06 15:23:25 -0400

    removed codecompanion

--- .config/nvim/lua/plugins/codecompanion.lua
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -1,19 +1,20 @@
-return {
-  "olimorris/codecompanion.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-treesitter/nvim-treesitter",
-  },
-  config = function()
-    require("codecompanion").setup({
-      strategies = {
-        chat = {
-          adapter = "anthropic",
-        },
-        inline = {
-          adapter = "anthropic",
-        },
-      },
-    })
-  end,
-}
+return {}
+-- return {
+--   "olimorris/codecompanion.nvim",
+--   dependencies = {
+--     "nvim-lua/plenary.nvim",
+--     "nvim-treesitter/nvim-treesitter",
+--   },
+--   config = function()
+--     require("codecompanion").setup({
+--       strategies = {
+--         chat = {
+--           adapter = "anthropic",
+--         },
+--         inline = {
+--           adapter = "anthropic",
+--         },
+--       },
+--     })
+--   end,
+-- }
